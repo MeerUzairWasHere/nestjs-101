75a220f3d44b8c3d757b1b18267fe845e076ed03
Meer Uzair <meer.uxair007@gmail.com>
2025-06-09 13:32:37 +0530

    Update index.ts

--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -6,7 +6,25 @@ import {
   MinLength,
 } from 'class-validator';
 
-export class AuthDto {
+export class loginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  password: string;
+}
+
+export class registerDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(20)
+  name: string;
+
   @IsEmail()
   @IsNotEmpty()
   email: string;
