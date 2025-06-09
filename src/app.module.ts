16dacc7af1b4140ff900b795ccefaf73a6b561d8
Meer Uzair <meer.uxair007@gmail.com>
2025-06-09 11:22:56 +0530

    added PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, PrismaModule, BookmarkModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
