84e9d4248d7683f129c4654b33fbdcf888724ed9
Meer Uzair <meer.uxair007@gmail.com>
2025-06-14 23:36:53 +0530

    added cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
